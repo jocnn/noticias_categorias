89ba159645acc0f236338519961502a02cd275cc
jocnn <ijann_@outlook.com>
2022-05-19 10:19:45 -0300

    consultando segun categoria

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,10 +1,26 @@
 import { createContext, useState, useEffect } from 'react'
+import axios from 'axios'
 
 const NoticiasContext = createContext()
 
 const NoticiasProvider = ({children}) => {
 
   const [ categoria, setCategoria ] = useState('general')
+  const [ noticias, setNoticias ] = useState([])
+
+  useEffect(() => {
+    
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&pageSize=100&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+
+      const { data } = await axios(url)
+
+      setNoticias(data.articles)
+      
+    }
+
+    consultarAPI()
+  }, [categoria])
 
   const handleChangeCategoria = e => {
     setCategoria(e.target.value)

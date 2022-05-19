81fb8f6a9b4be579d0fb1d68a7a7e18e699d077d
jocnn <ijann_@outlook.com>
2022-05-19 09:35:06 -0300

    creando el provider y usandolo en app

---
+++ src/context/NoticiasProvider.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState, useEffect } from 'react'
+
+const NoticiasContext = createContext()
+
+const NoticiasProvider = ({children}) => {
+
+  return (
+    <NoticiasContext.Provider 
+      value={{
+        
+      }}
+    >
+      {children}
+    </NoticiasContext.Provider>
+  )
+}
+
+export {
+  NoticiasProvider
+}
+
+export default NoticiasContext
